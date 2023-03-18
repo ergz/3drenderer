ab0e7746c9d01dc4e78deadc4199b21e44a30885
ergz <e.emanuel.rodriguez@gmail.com>
2023-03-18 11:19:31 -0700

    updates the build file

--- build.bat
+++ build.bat
@@ -8,5 +8,3 @@ set sdl_lib_path="D:\dev\libraries\SDL2-devel-2.24.2-VC\SDL2-2.24.2\lib\x64"
 set output_dir="build\"
 
 cl /nologo /Zi /Fd"build/" /Fo"build/" /Fe"build/renderer.exe" /I %sdl_inc_path% src\*.c SDL2.lib SDL2main.lib shell32.lib /link /SUBSYSTEM:CONSOLE /LIBPATH:%sdl_lib_path% 
-
-
